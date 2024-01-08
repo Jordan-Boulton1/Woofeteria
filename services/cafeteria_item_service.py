b266761bc8974f41e4b54455fbf7fa54dcc2af65
Jordan <127216241+Jordan-Boulton1@users.noreply.github.com>
2024-01-08 11:43:06 +0000

    feat: #6 add comments to cafeteria item service

--- services/cafeteria_item_service.py
+++ services/cafeteria_item_service.py
@@ -10,25 +10,44 @@ class CafeteriaItemService:
         self.price_converter = PriceConverter()
 
     def print_cafeteria_menu(self):
+        """
+        Prints the menu to the console.
+        """
         for item in self.cafeteria_items:
             print(f"{item.Id}. {item.Name} - £{self.price_converter.format_price(item.Price)} | Stock - x{item.Stock}")
 
     def get_cafeteria_menu(self):
+        """
+        Returns the list of cafeteria menu items.
+        """
         return self.cafeteria_items
 
     def subtract_from_stock(self, ordered_item: CafeteriaItem, menu_list: list[CafeteriaItem], ordered_amount: int):
+        """
+        Loops through the menu list and checks if the id of the item passed in the function matches the current item id
+        in the loop, if it does it subtract the ordered amount from the current item stock and reassigns the menu to
+        the newly updated one.
+        """
         for cafeteria_item in menu_list:
             if ordered_item.Id == cafeteria_item.Id:
                 cafeteria_item.Stock -= ordered_amount
         self.cafeteria_items = menu_list
 
     def add_to_stock(self, ordered_item: CafeteriaItem, menu_list: list[CafeteriaItem], ordered_amount: int):
+        """
+        Loops through the menu list and checks if the id of the item passed in the function matches the current item id
+        in the loop, if it does it add the ordered amount to the current item stock and reassigns the menu to
+        the newly updated one.
+        """
         for cafeteria_item in menu_list:
             if ordered_item.Id == cafeteria_item.Id:
                 cafeteria_item.Stock += ordered_amount
         self.cafeteria_items = menu_list
 
     def __populate_cafeteria_menu(self):
+        """
+        Populates the menu with initial values.
+        """
         menu = [
             CafeteriaItem(1, "Woofin", 2.50, 10),
             CafeteriaItem(2, "Paw Cake", 3.20, 10),
