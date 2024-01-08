a86251ac1bec9dd91063fbd6a475beb755ff5874
Jordan <127216241+Jordan-Boulton1@users.noreply.github.com>
2024-01-08 11:57:16 +0000

    feat: #6 add comments to user input validator

--- infrastructure/validators/user_input_validator.py
+++ infrastructure/validators/user_input_validator.py
@@ -8,10 +8,21 @@ from entities.cafeteria_item import CafeteriaItem
 class UserInputValidator:
     @staticmethod
     def create_array_from_user_input(user_input: str):
+        """
+        Splits each character, if it is an integer, in the user input string into an array of integers.
+        """
         return [int(x) for x in user_input.split(',') if x.strip().isdigit()]
 
     @staticmethod
     def validate_user_input_is_comma_separated(user_input: str, array_length: int):
+        """
+        If the user is an empty string or whitespace, returns false.
+        Otherwise, creates a regex pattern that will accept only integers from 1 until the array length provided.
+        If the user input contains more than one integer from 1 until the array length provided the pattern expects the
+        integers to be separated by comma with optional space inbetween.
+        After that it matches the user input against the regex pattern and returns a boolean value depending on the
+        matching evaluation.
+        """
         if len(user_input) == 0:
             return False
         pattern = re.compile(r'^(?:[1-' + str(array_length) + '](?:, ?[1-' + str(array_length) + '])*)?$')
@@ -19,12 +30,21 @@ class UserInputValidator:
 
     @staticmethod
     def validate_user_input_is_a_number(user_input: str):
+        """
+        If the user is an empty string or whitespace, returns false.
+        Returns a boolean value based on the evaluation if the user input is a digit.
+        """
         if len(user_input) == 0:
             return False
         return bool(user_input.isdigit())
 
     @staticmethod
     def validate_user_input_is_a_decimal(user_input: str):
+        """
+        If the user is an empty string or whitespace, returns false.
+        Replaces the "." in the user input string with an empty string and checks if the result of that is numeric.
+        If it is numeric it returns true otherwise, it returns false.
+        """
         if len(user_input) == 0:
             return False
         elif user_input.replace(".", "").isnumeric():
@@ -34,6 +54,13 @@ class UserInputValidator:
 
     @staticmethod
     def validate_input_before_parsing(cart, is_removing: bool = False):
+        """
+        Replaces the displayed text in the console depending on if the user is removing or adding an item then creates a
+        while loop which validates if the provided input contains integers and if more than one they are separated by
+        comma.
+        If the validation fails the user will be prompted with a message stating the input is invalid.
+        Otherwise, the loop will break and return the user input.
+        """
         info_text = (
             "Please enter the relevant number from the menu, that corresponds to the item you wish to order.\n "
             "If you wish to order more than one item please separate each item number by comma.")
@@ -52,6 +79,13 @@ class UserInputValidator:
 
     @staticmethod
     def validate_input_for_items(item: CafeteriaItem, is_removing: bool = False):
+        """
+        Replaces the displayed text in the console depending on if the user is removing or adding an item then creates
+        a while loop that prompts the user how much of the selected item they wish to order or remove and validates if
+        the provided input contains integers and if more than one they are separated by comma.
+        If the validation fails the user will be prompted with a message stating the input is invalid.
+        Otherwise, the loop will break and return the user input.
+        """
         info_text = "order"
         if is_removing:
             info_text = "remove"
