9ca53e3a2e6a4f172d2d080ac03ec35a4a08effa
Jordan <127216241+Jordan-Boulton1@users.noreply.github.com>
2024-01-08 11:58:04 +0000

    feat: #6 add comment to price converter

--- infrastructure/helpers/price_converter.py
+++ infrastructure/helpers/price_converter.py
@@ -2,4 +2,7 @@ from dataclasses import dataclass
 @dataclass
 class PriceConverter:
     def format_price(self, price: float):
+        """
+        Formats the price to 2 decimal places.
+        """
         return "{0:.2f}".format(price)
