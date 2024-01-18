2ca59b186129f02aa2ab0cce32bf1acb4364f81d
Jordan <127216241+Jordan-Boulton1@users.noreply.github.com>
2024-01-18 20:47:28 +0000

    feat: update #13 cart service docstrings

--- services/cart_service.py
+++ services/cart_service.py
@@ -12,8 +12,14 @@ class CartService:
 
     def add_to_cart(self, selected_items: list[CafeteriaItem]):
         """
-        Creates a new cart object with the ordered items from the user and
-        calculated quantity and price.
+        Create a new Cart object with the provided list of CafeteriaItems.
+        Args:
+            selected_items (list[CafeteriaItem]): A list of CafeteriaItems to
+            be added to the cart.
+        Returns:
+            Cart: A new Cart object initialized with the provided
+            selected_items, total quantity, and total price calculated from
+            the selected_items.
         """
         cart = Cart(selected_items,
                     self.__calculate_total_quantity(selected_items),
@@ -22,9 +28,17 @@ class CartService:
 
     def update_cart(self, cart: Cart, selected_items: list[CafeteriaItem]):
         """
-        Recalculates the total quantity and price of the cart from the newly
-        selected items.
-        """
+       Update the provided Cart object with information from a list of selected
+       CafeteriaItems.
+       Args:
+           cart (Cart): The Cart object to be updated.
+           selected_items (list[CafeteriaItem]): A list of CafeteriaItems
+           to be added to the cart.
+       Returns:
+           Cart: The updated Cart object containing the total price,
+           total quantity, and a sorted list of selected CafeteriaItems
+           based on their IDs.
+       """
         cart.TotalPrice = self.__calculate_total_price(selected_items)
         cart.TotalQuantity = self.__calculate_total_quantity(selected_items)
         cart.Items = selected_items
@@ -33,15 +47,28 @@ class CartService:
 
     def remove_from_cart(self, cart: Cart, item_id: int):
         """
-        Filters out the item from the cart that is to be removed and updates
-        the cart.
+        Remove a CafeteriaItem with the specified item ID from the given Cart.
+        Args:
+            cart (Cart): The Cart object from which the item will be removed.
+            item_id (int): The ID of the CafeteriaItem to be removed from the
+            cart.
+        Returns:
+            Cart: The updated Cart object after removing the specified
+            CafeteriaItem.
         """
         cart.Items = [item for item in cart.Items if item.Id != item_id]
         return self.update_cart(cart, cart.Items)
 
     def print_cart(self, cart: Cart):
         """
-        Prints the details of the cart to the console.
+        Print the contents of a Cart including total quantity, total price,
+        and item details.
+        Args:
+            cart (Cart): The Cart object to be printed.
+        Prints:
+            The total quantity and total price of items in the cart, along
+            with details of each CafeteriaItem including its ID, name, price
+            and stock quantity.
         """
         print(f"Total Quantity: {cart.TotalQuantity}")
         print(f"Total Price: £"
@@ -53,17 +80,29 @@ class CartService:
 
     def __calculate_total_quantity(self, selected_items: list[CafeteriaItem]):
         """
-        Calculates the total quantity of the cart by using the items inside it.
+        Calculate the total quantity of a list of CafeteriaItems based on
+        their stock quantities.
+        Args:
+           selected_items (list[CafeteriaItem]): A list of CafeteriaItems for
+           which to calculate the total quantity.
+       Returns:
+           int: The total quantity calculated by summing the individual stock
+           quantities of each item in the selected_items list.
         """
         return sum(item.Stock for item in selected_items)
 
     def __calculate_total_price(self, selected_items: list[CafeteriaItem]):
         """
-        Creates an empty array that will store the total price * order
-        quantity per item in the cart, and loops through the list of ordered
-        items, calculates the price per item and adds it to the array.
-        Finally sums up the total price per item.
-        """
+       Calculate the total price of a list of CafeteriaItems based on their
+       prices and quantities.
+       Args:
+           selected_items (list[CafeteriaItem]): A list of CafeteriaItems for
+           which to calculate the total price.
+       Returns:
+           float: The total price calculated by summing the individual total
+           prices of each item (price multiplied by quantity) in the
+           selected_items list.
+       """
         total_price_per_item_array = []
         for selected_item in selected_items:
             total_price_per_item = selected_item.Price * selected_item.Stock
